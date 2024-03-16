1aee2b074ab09975c1a319a5e13d6cda0fbac24d
Nandhakishore23 <131781031+Nandhakishore23@users.noreply.github.com>
2024-03-16 11:52:31 +0530

    Update p12.py

--- py/p12.py
+++ py/p12.py
@@ -1,30 +1,19 @@
 # Program to multiply two matrices using nested loops
 
+import sys
+
 A = [[1,2,3],[4,5,6],[7,8,9]]
 B = [[10,11,12],[13,14,15],[16,17,18]]
- 
+
 p = len(A)
 q = len(A[0])
- 
 t = len(B)
 r = len(B[0])
- 
-if(q!=t):
+
+if q != t:
    print("Error! Matrix sizes are not compatible")
-   quit()
+   sys.exit()
 
-C = []
-for row in range(p):
-   curr_row = []
-   for col in range(r):
-       curr_row.append(0)
-   C.append(curr_row)
+C = [[sum(A[i][k] * B[k][j] for k in range(q)) for j in range(r)] for i in range(p)]
 
-for i in range(p):
-   for j in range(r):
-       curr_val = 0
-       for k in range(q):
-           curr_val += A[i][k]*B[k][j]
-       C[i][j] = curr_val
- 
 print(C)
