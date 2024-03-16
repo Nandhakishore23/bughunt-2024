74dc517cc6d5a2565cc8d72b5c984c96a490bbac
Nandhakishore23 <131781031+Nandhakishore23@users.noreply.github.com>
2024-03-16 11:48:38 +0530

    Update p7.py

--- py/p7.py
+++ py/p7.py
@@ -14,21 +14,21 @@ class Solution:
         tail = dummyHead
         carry = 0
 
-        while l1 is not None or l2 is not None or carry != 0:
-            digit1 = l1.val if l1 is not None else 0
-            digit2 = l2.val if l2 is not None else 0
+        while l1 or l2 or carry:
+            digit1 = l1.val if l1 else 0
+            digit2 = l2.val if l2 else 0
 
-            sum = digit1 + digit2 + carry
-            digit = sum % 10
-            carry = sum // 10
+            total_sum = digit1 + digit2 + carry
+            digit = total_sum % 10
+            carry = total_sum // 10
 
             newNode = ListNode(digit)
             tail.next = newNode
             tail = tail.next
 
-            l1 = l1.next if l1 is not None else None
-            l2 = l2.next if l2 is not None else None
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
 
-        result = dummyHead.next
-        dummyHead.next = None
-        return result
+        return dummyHead.next
