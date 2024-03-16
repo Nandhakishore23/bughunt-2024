0d459c376fb601f34dd14f6ce921557781d485f0
Nandhakishore23 <131781031+Nandhakishore23@users.noreply.github.com>
2024-03-16 10:22:50 +0530

    Update p1.py

--- py/p1.py
+++ py/p1.py
@@ -4,13 +4,15 @@
 #           ***
 #          *****
 
-def asterisk_tree(height,level):
-  if level > height:
-    return
-  for j in range(2*level-1):
-    print("*", end="")
-  print()
-  asterisk_tree(height, level + 1)
+def asterisk_tree(height, level):
+    if level > height:
+        return
+    for j in range(height - level):
+        print(" ", end="")
+    for j in range(2 * level - 1):
+        print("*", end="")
+    print()
+    asterisk_tree(height, level + 1)
 
-n=int(input("Enter height of tree : "))
+n = int(input("Enter height of tree: "))
 asterisk_tree(n, 1)
